971cf06bcfb1e0e39b90067b243c252a20f7f372
ShezzTRANS <kebua.iti5@gmail.com>
2026-03-27 21:15:44 +0200

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Chat(Base):
-    __tablename__ = "chats"
-
-    id = Column(Integer, primary_key=True, index=True)
-    prompt = Column(String, index=True)
-    response = Column(String)
